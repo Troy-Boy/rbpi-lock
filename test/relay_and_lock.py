df27267ad8e66102e516b2bfafcdabc91c6ac023
Troy-Boy <achille.lanctots@gmail.com>
2024-06-01 18:03:48 -0400

    further realy tests

--- test/relay_and_lock.py
+++ test/relay_and_lock.py
@@ -1,21 +1,24 @@
 import RPi.GPIO as GPIO
 import time
 
-# Set up the GPIO pin
-relay_pin = 26  # Change to your GPIO pin for the relay
+# GPIO pin where the relay is connected
+relay_pin = 26
+
+# Setup
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(relay_pin, GPIO.OUT)
 
-def activate_relay(duration=1):
-    """Activate the relay for a specified duration in seconds."""
-    GPIO.output(relay_pin, GPIO.HIGH)  # Energize the relay (lock open)
-    time.sleep(duration)
-    GPIO.output(relay_pin, GPIO.LOW)  # De-energize the relay (lock closed)
+def activate_relay(duration=5):
+    GPIO.output(relay_pin, GPIO.HIGH)  # Energize the relay
+    print("Relay activated, lock should be open.")
+    time.sleep(duration)  # Keep it energized for 'duration' seconds
+    GPIO.output(relay_pin, GPIO.LOW)   # De-energize the relay
+    print("Relay deactivated, lock should be closed.")
 
 try:
     while True:
-        activate_relay(5)  # Activate for 5 seconds
-        time.sleep(10)  # Wait for 10 seconds
+        activate_relay(5)  # Activate the relay for 5 seconds
+        time.sleep(10)  # Wait for 10 seconds before the next cycle
 except KeyboardInterrupt:
     pass
 finally:
