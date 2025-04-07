f46f95a2c0551747ba556fad41702542b2d68e4a
Troy-Boy <achille.lanctots@gmail.com>
2025-04-07 10:47:17 -0400

    print statement better

--- servo_test.py
+++ servo_test.py
@@ -25,16 +25,16 @@ def main():
     pwm.start(0)
 
     try:
-        print("🔧 Moving to 0°")
+        print("Moving to 0°...")
         set_angle(pwm, 0)
         time.sleep(1)
 
-        print("Rotating to 180°")
+        print("Rotating to 180°...")
         set_angle(pwm, 180)
         time.sleep(1)
 
     finally:
-        print("🧼 Cleaning up")
+        print("Cleaning up")
         pwm.stop()
         GPIO.cleanup()
 
